8baa3a66e73546a8bae16948427bdd1906e45e71
Nelson <nelson.vico@gmail.com>
2024-05-02 11:09:22 +0200

    auth

--- auth/indexAuth.js
+++ auth/indexAuth.js
@@ -8,7 +8,7 @@ const port = 3500;
 
 app
 .use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://lbo-clone.vercel.app',
     methods: ['GET','POST'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
